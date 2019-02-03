b83e58ba6a9049b68fd607dc34356abcfa143408
Rebecca Ao <rbccaao@gmail.com>
2019-02-03 16:20:08 +1100

    Banner and navigation bar tweaking
    Mostly sizing changes, updated the colours for the navbar

--- js/banner-sticky.js
+++ js/banner-sticky.js
@@ -2,7 +2,7 @@ function resizeHeaderOnScroll() {
 	const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 	documentHeaders = document.getElementsByTagName('header');
 	header = documentHeaders[0];
-	breakpoint = header.offsetHeight * 0.66;
+	breakpoint = header.offsetHeight * 0.5;
 
 	if (scrollPosition > breakpoint) {
 		header.classList.add("sm", "sticky-top");
