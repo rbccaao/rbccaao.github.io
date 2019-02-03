3cf338d12e9f5bc9fe5fb67d0330d814f20e4c68
Rebecca Ao <rbccaao@gmail.com>
2019-02-03 11:43:12 +1100

    Banner and navigation bar tweaking
    Changed colour scheme to greyscale

--- js/banner-sticky.js
+++ js/banner-sticky.js
@@ -2,7 +2,7 @@ function resizeHeaderOnScroll() {
 	const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 	documentHeaders = document.getElementsByTagName('header');
 	header = documentHeaders[0];
-	breakpoint = header.offsetHeight * 0.5;
+	breakpoint = header.offsetHeight * 0.66;
 
 	if (scrollPosition > breakpoint) {
 		header.classList.add("sm", "sticky-top");
