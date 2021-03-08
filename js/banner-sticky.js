5af833ea85be9572094a4ca9f26c87422571d618
rbccaao <42102397+rbccaao@users.noreply.github.com>
2021-03-08 18:30:59 +0900

    Banner behaviour tweak
    -adjusted the size and trigger on scroll variables

--- js/banner-sticky.js
+++ js/banner-sticky.js
@@ -2,12 +2,13 @@ function resizeHeaderOnScroll() {
 	const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 	documentHeaders = document.getElementsByTagName('header');
 	header = documentHeaders[0];
-	breakpoint = header.offsetHeight * 0.5;
+	breakpoint = header.offsetHeight * 0.45;
 
 	if (scrollPosition > breakpoint) {
 		header.classList.add("sm", "sticky-top");
 	} else {
 		header.classList.remove("sm", "sticky-top");
+		window.scrollTo(0,0);
 	}
 }
 
